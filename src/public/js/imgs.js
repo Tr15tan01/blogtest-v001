365b0f5b7119369d60a3fd9c15e1f2d1aa603a31
titevar <59051464+titevar@users.noreply.github.com>
2020-10-06 15:00:15 +0400

    changing image blur settings

--- src/public/js/imgs.js
+++ src/public/js/imgs.js
@@ -1,8 +1,4 @@
 const img = document.getElementsByClassName('masthead')[0]
 
-window.addEventListener('load', (event) => {
-    console.log('page is fully loaded');
-
     img.classList.add('is-loaded')  
     
-  });
